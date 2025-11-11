8a649008b46b737408f6386b8e70df0f0723d5e0
Prabhakar-more <prabhakarmore0403@gmail.com>
2025-11-11 12:27:39 +0530

    initial commit

---
+++ Functional Interface/src/com/pm/fp_basics/ConsumerExample1.java
@@ -0,0 +1,31 @@
+package com.pm.fp_basics;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample1 {
+    public static void main(String[] args) {
+
+        // Step 1: Define Consumer
+        Consumer<String> printWelcome = name -> System.out.println("Welcome " + name.toUpperCase());
+
+        // Step 2: Create a list of names
+        List<String> names = List.of("Amit", "Neha", "Ravi");
+
+        // Step 3: Apply Consumer without streams
+        for (String name : names) {
+            printWelcome.accept(name);  // Consumer in action
+        }
+    }
+}
+
+
+
+/*
+Consumer
+
+Create a Consumer<String> that prints a name in uppercase with "Welcome " before it.
+
+Use it with 3 names in a list ("Amit", "Neha", "Ravi").
+
+*/
